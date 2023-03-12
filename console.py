698de59cd5281e790c97d8eb854be16d85d89ac6
Sally-N <snzungula@gmail.com>
2023-03-12 17:04:51 +0000

    small issues

--- console.py
+++ console.py
@@ -69,8 +69,68 @@ class HBNBCommand(cmd.Cmd):
                 if key not in storage.all():
                     print("** no instance found **")
                 else:
-                    del stoarge.all()key[]
+                    del storage.all()key[]
                     storage.save()
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        if arg != "":
+            args = arg.split('')
+            if args[0] not in storage.classes:
+                print("** class doesn't exist **")
+            else:
+                string_rep = [str(obj) for key, obj in storage.all().items()
+                        if type(obj).obj_cls_name == args[0]]
+                print(string_rep)
+        else:
+            new_s = [str(obj) for key, obj in storage.all().items]
+            print(new_s)
+
+    def do_update(self, arg):
+        """Updates an instance based on classname and id"""
+        if arg == "" or arg is None:
+            print("** clase name is missing **")
+            return
+
+        regex = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match = re.search(regex, arg)
+        classname = match.group(1)
+        uid = match.group(2)
+        attribute = match.group(3)
+        value = match(4)
+        if not match:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id is missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            elif not attribute:
+                print("** attribute name mising **")
+            elif not value:
+                print("** value missing **")
+            else:
+                cast = None
+                if not re.search('^".*"$', value):
+                    if '.' in value:
+                        cast = float
+                    else:
+                        cast = int
+                else:
+                    value = value.replace('"', '')
+                attributes = storage.attributes()[classname]
+                if attribute in attributes:
+                    value = attributes[attribute](value)
+                elif cast:
+                    try:
+                        value = cast(value)
+                    except ValueError:
+                        pass
+                setattr(storage.all()key], attriute, value)
+                storage.all()[key].save()
 
 
 if __name__ == '__main__':
